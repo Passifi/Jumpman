29ac3a562b485629d7b2aa6ca5a72c92306bfe4f
Passifi <pascal.wigger@gmx.de>
2023-03-03 10:56:44 +0100

    collisionDetection and better jumping

--- main.js
+++ main.js
@@ -10,11 +10,44 @@ const ctx = screen.getContext("2d");
 const GRAVITY = 1; 
 const TERMINAL_VELOCITY = 20;
 let x,y;
+const SCREEN_WIDTH = screen.width;
+const SCREEN_HEIGH = screen.height;
 x=20;
 y=20;
 velocity = 0;
 yVelocity = 0;
 setInterval(mainLoop,12)
+
+class Point {
+    constructor(x,y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Box {
+    constructor(x,y,width,height)
+    {
+        this.leftBoundary = x;
+        this.topBoundary = y;
+        this.rightBoundary = x + width;
+        this.bottomBoundary = y+height;
+        this.width = width/2;
+        this.height = height/2;
+        this.centerPoint = new Point(x + width/2, y + height/2);
+    }
+
+    returnPoints()
+    {
+        let pointArray = [];
+        pointArray.push(new Point(this.leftBoundary, this.topBoundary));
+        pointArray.push(new Point(this.rightBoundary, this.topBoundary));
+        pointArray.push(new Point(this.leftBoundary, this.bottomBoundary));
+        pointArray.push(new Point(this.rightBoundary, this.bottomBoundary));
+        return pointArray;
+    }
+}
 
 class Plattform {
     constructor(x,y,w,h){
@@ -26,11 +59,18 @@ class Plattform {
 
     move() {
         this.x += 3;
+        if(this.x > SCREEN_WIDTH)
+            this.x = 0;
+
+        
     }
 }
 
 var plattforms = [];
 plattforms.push(new Plattform(12,300,90,10));
+
+
+
 
 function sgn(num)
 {
@@ -71,6 +111,30 @@ function applyVelocity()
     
 }
 
+function pointInside(point, box)
+{
+    if(point.x > box.leftBoundary && point.x < box.rightBoundary)
+    {
+        if(point.y > box.topBoundary && point.y < box.bottomBoundary)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+function centerPointMethod(box1, box2)
+{
+    let centerDeltaX = Math.abs(box1.centerPoint.x - box2.centerPoint.x);
+    let centerDeltaY = Math.abs(box1.centerPoint.y - box2.centerPoint.y);
+    let maxDistanceX = box1.width + box2.width;
+    let maxDistanceY = box1.height + box2.height;
+    if(centerDeltaX <= maxDistanceX)
+        if(centerDeltaY <= maxDistanceY)
+            return true;
+    return false;
+}
+
 function collisionDetection() 
 {
     if(x < 0 || x + 40 > screen.width)
@@ -80,6 +144,35 @@ function collisionDetection()
         x += sgn(velocity)*xDelta;
 
     }
+    let collisionDetected = false;
+    playerBox = new Box(x,y,40,40);
+    playerPoints = playerBox.returnPoints();
+
+    for (let i = 0; i < plattforms.length; i++) {
+        const element = plattforms[i];
+        pattformBox = new Box(element.x,element.y,element.w,element.h);
+        for(let k =0; k < playerPoints.length; k++)
+        {
+            //collisionDetected = pointInside(playerPoints[k],pattformBox);
+            collisionDetected = centerPointMethod(playerBox,pattformBox);
+            
+            if(collisionDetected)
+            {
+                yVelocity *= -1;
+            }
+        }
+        if(collisionDetected)
+            break;
+
+    }
+
+    touchingTheGround = collisionDetected;
+
+    if(y >= screen.height- 40)
+        touchingTheGround = true;
+    
+        
+            
 }
 
 function mainLoop()
@@ -94,8 +187,6 @@ function mainLoop()
     applyGravity();
     applyVelocity();
    
-    if(y >= screen.height- 40)
-        touchingTheGround = true;
     
     drawBox(x,y); 
     plattforms.forEach(element => {
